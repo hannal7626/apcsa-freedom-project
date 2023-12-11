67681bf6e8b162cb353bfa86851125a814f12c4f
hannal7626 <91745114+hannal7626@users.noreply.github.com>
2023-12-11 01:49:34 -0500

    Add files via upload

---
+++ EmojisApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojisApp.swift
+//  Emojis
+//
+//  Created by hanna li on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
