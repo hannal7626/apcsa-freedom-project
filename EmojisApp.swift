936461021d450a469e6de8b73ccac305761e96be
hannal7626 <91745114+hannal7626@users.noreply.github.com>
2023-12-11 01:52:42 -0500

    Update EmojisApp.swift

--- EmojisApp.swift
+++ EmojisApp.swift
@@ -7,11 +7,34 @@
 
 import SwiftUI
 
-@main
-struct EmojisApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+enum Emoji: String, CaseIterable {
+    case fairy = "🧚🏻‍♀️"
+    case cake = "🍰"
+    case swan = "🦢"
+    case bouquet = "💐"
+}
+
+struct ContentView: View {
+    @State var selection: Emoji = .cake
+    
+    var body: some View {
+        VStack{
+            Text(selection.rawValue)
+                .font(.system(size: 150))
+            
+            Picker("Select Emoji", selection: $selection) {
+                ForEach(Emoji.allCases, id: \.self) {emoji in
+                    Text(emoji.rawValue)
+                }
+            }
+            .pickerStyle(.segmented)
         }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View{
+        ContentView()
     }
 }
